420fec6575d17d081d9028c9e13992146976f9a2
Alim2301d <133909516+Alim2301d@users.noreply.github.com>
2023-12-01 16:55:19 +0500

    all files

---
+++ eproject-Ali/resources/views/main-web/components/nav.blade.php
@@ -0,0 +1,122 @@
+<header class="header">
+
+<div class="header-top">
+<div class="container">
+<div class="header-top-wrapper">
+<div class="header-top-left">
+<div class="header-top-contact">
+<ul>
+<li><a href="#"><i class="far fa-location-dot"></i> 25/B Milford Road, New York</a></li>
+<li><a href="/cdn-cgi/l/email-protection#c6afa8a0a986a3bea7abb6aaa3e8a5a9ab"><i class="far fa-envelopes"></i>
+<span class="__cf_email__" data-cfemail="eb82858d84ab8e938a869b878ec5888486">[email&#160;protected]</span></a></li>
+<li><a href="tel:+21236547898"><i class="far fa-phone-volume"></i> +2 123 654 7898</a>
+</li>
+</ul>
+</div>
+</div>
+<div class="header-top-right">
+<div class="header-top-social">
+<span>Follow Us: </span>
+<a href="#"><i class="fab fa-facebook"></i></a>
+<a href="#"><i class="fab fa-twitter"></i></a>
+<a href="#"><i class="fab fa-instagram"></i></a>
+<a href="#"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="main-navigation">
+<nav class="navbar navbar-expand-lg">
+<div class="container position-relative">
+<a class="navbar-brand" href="index.html">
+<img src="assets/img/logo/logo.png" alt="logo">
+</a>
+<div class="mobile-menu-right">
+<div class="search-btn">
+<button type="button" class="nav-right-link"><i class="far fa-search"></i></button>
+</div>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
+</button>
+</div>
+<div class="collapse navbar-collapse" id="main_nav">
+<ul class="navbar-nav">
+<li class="nav-item dropdown">
+<a class="nav-link  active" href="" data-bs-toggle="dropdown">Home</a>
+
+</li>
+<li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Services</a>
+<ul class="dropdown-menu fade-down">
+<li><a class="dropdown-item" href="service.html">Services One</a></li>
+<li><a class="dropdown-item" href="service-2.html">Services Two</a></li>
+<li><a class="dropdown-item" href="service-single.html">Service Single</a></li>
+</ul>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Pages</a>
+<ul class="dropdown-menu fade-down">
+<li><a class="dropdown-item" href="about.html">About Us</a></li>
+<li><a class="dropdown-item" href="team.html">Our Team</a></li>
+<li><a class="dropdown-item" href="gallery.html">Gallery</a></li>
+<li><a class="dropdown-item" href="contact.html">Contact Us</a></li>
+<li class="dropdown-submenu">
+<a class="dropdown-item dropdown-toggle" href="#">Authentication</a>
+<ul class="dropdown-menu">
+<li><a class="dropdown-item" href="login.html">Login</a></li>
+<li><a class="dropdown-item" href="register.html">Register</a></li>
+<li><a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
+</li>
+</ul>
+</li>
+<li><a class="dropdown-item" href="pricing.html">Pricing Plan</a></li>
+<li><a class="dropdown-item" href="faq.html">Faq</a></li>
+<li><a class="dropdown-item" href="testimonial.html">Testimonials</a></li>
+<li><a class="dropdown-item" href="404.html">404 Error</a></li>
+<li><a class="dropdown-item" href="coming-soon.html">Coming Soon</a></li>
+<li><a class="dropdown-item" href="terms.html">Terms Of Service</a></li>
+<li><a class="dropdown-item" href="privacy.html">Privacy Policy</a></li>
+</ul>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Portfolio</a>
+<ul class="dropdown-menu fade-down">
+<li><a class="dropdown-item" href="portfolio.html">Portfolio One</a></li>
+<li><a class="dropdown-item" href="portfolio-2.html">Portfolio Two</a></li>
+<li><a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a></li>
+</ul>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Blog</a>
+<ul class="dropdown-menu fade-down">
+<li><a class="dropdown-item" href="blog.html">Blog</a></li>
+<li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
+</ul>
+</li>
+<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+</ul>
+<div class="nav-right">
+<div class="search-btn">
+<button type="button" class="nav-right-link"><i class="far fa-search"></i></button>
+</div>
+<div class="nav-right-btn mt-2">
+<a href="contact.html" class="theme-btn">Get A Quote<i class="fas fa-arrow-right-long"></i></a>
+</div>
+</div>
+</div>
+
+<div class="search-area">
+<form action="#">
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Type Keyword...">
+<button type="submit" class="search-icon-btn"><i class="far fa-search"></i></button>
+</div>
+</form>
+</div>
+
+</div>
+</nav>
+</div>
+</header>
---
+++ eproject-Ali/resources/views/main-web/index.blade.php
@@ -0,0 +1,10 @@
+@extends('main-web.components.master')
+
+@section('content')
+@include('main-web.components.nav')
+@include('main-web.components.main')
+@include('main-web.components.footer')
+
+
+
+@endsection
