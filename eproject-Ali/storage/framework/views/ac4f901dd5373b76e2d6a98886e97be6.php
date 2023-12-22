ed43bb2add46860119074b27a136e9150bd47fa7
hamzamalik09 <133909516+Alim2301d@users.noreply.github.com>
2023-12-22 10:58:51 +0500

    COMMI09

--- eproject-Ali/storage/framework/views/ac4f901dd5373b76e2d6a98886e97be6.php
+++
@@ -1,2 +0,0 @@
-<input type="checkbox" <?php echo $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500']); ?>>
-<?php /**PATH C:\Users\IDEAL COMPUTER'S\Documents\GitHub\lab-automation\eproject-Ali\resources\views/components/checkbox.blade.php ENDPATH**/ ?>
